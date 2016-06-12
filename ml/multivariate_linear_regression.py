1cfa7511fe17c2f7ab3da9a99eb8da50dccd5ed4
Matt Ball <matt.ball.2@gmail.com>
2016-06-12 12:40:31 -0700

    normalizes input data; adjusts cost fn name

--- ml/multivariate_linear_regression.py
+++ ml/multivariate_linear_regression.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Create some sample data.
+# Create some sample data and normalize it.
 x1 = np.array([[100, 125, 300, 400, 500, 524, 423, 455, 788, 322, 221, 282]])
-x1 = x1.astype('float32') / np.max(x1)
+x1 = (x1.astype('float32') - np.mean(x1)) / np.std(x1)
 
 x2 = np.array([[323, 424, 444, 211, 100, 520, 393, 188, 887, 42,  100, 449]])
-x2 = x2.astype('float32') / np.max(x2)
+x2 = (x2.astype('float32') - np.mean(x2)) / np.std(x2)
 
 x3 = np.array([[100, 900, 800, 770, 232, 100, 400, 448, 22,  100, 300, 200]])
-x3 = x3.astype('float32') / np.max(x3)
+x3 = (x3.astype('float32') - np.mean(x3)) / np.std(x3)
 
 x4 = np.array([[400, 200, 999, 188, 377, 222, 111, 33,  100, 100, 23,  388]])
-x4 = x4.astype('float32') / np.max(x4)
+x4 = (x4.astype('float32') - np.mean(x4)) / np.std(x4)
 
 ones_vector = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
 x = np.concatenate((ones_vector, x1, x2, x3, x4))
@@ -38,7 +38,7 @@ def h(weights_vector, x_matrix):
   return np.dot(weights_vector, x_matrix)
 
 
-def cost(weights_vector):
+def linear_cost(weights_vector):
   summation = ((h(weights_vector, x) - y) ** 2).sum()
   samples = x1.shape[1]
   return summation / (2 * samples)
@@ -55,10 +55,10 @@ weights = np.random.rand(1, x.shape[0])
 
 # Setup the learning rate and iterate on the weights.
 learning_rate = 1e-1
-iterations = 1e3
+iterations = 1e2
 costs = []
 while True:
-  current_cost = cost(weights)
+  current_cost = linear_cost(weights)
   print 'cost:', current_cost
   # Improve the weights.
   partial_derivatives = dJdW(weights)
