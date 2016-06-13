80895b182a3cbc3f2612885d7cb60a25ecc4e450
Matt Ball <matt.ball.2@gmail.com>
2016-06-12 18:28:23 -0700

    fixes typo; adds radius param

--- ml/binary_logistic_regression.py
+++ ml/binary_logistic_regression.py
@@ -7,20 +7,23 @@ import numpy as np
 # A donut dataset centered around the origin.
 x1 = [-3, -2, -1,  0, 1, 2, 3, -30, -20, -10, 0,  10,  20,  30, 15, -10, 0]
 x2 = [-2, -4, -1, -1, 3, 1, 2, -25,  30,  10, 30, 40, -20, -10, 15, -20, -20]
+r = [(x1[i]**2 + x2[i]**2) ** 0.5 for i in range(len(x1))]
 ones_vector = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 x1 = np.array([x1])
 x2 = np.array([x2])
+r = np.array([r])
 ones_vector = np.array([ones_vector])
-# Note x is (3 x 17).
-x = np.concatenate((ones_vector, x1, x2))
+# Note x is (4 x 17).
+x = np.concatenate((ones_vector, x1, x2, r))
 
 y = [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 # Note y is (1 x 17).
 y = np.array([y])
+samples = x1.shape[1]
 
 
 def sigmoid(x):
-  return 1 / (1 + np.exp(x))
+  return 1 / (1 + np.exp(-1 * x))
 
 
 def h(weights_vector, x_matrix):
@@ -41,27 +44,25 @@ def h(weights_vector, x_matrix):
 def logistic_cost(weights_vector):
   summation = (np.dot(y, np.log(h(weights_vector, x)).T) +
                np.dot((1 - y), np.log(1 - h(weights_vector, x).T)))
-  samples = x1.shape[1]
-  return summation.flatten()[0] / samples
+  return -1 * summation.flatten()[0] / samples
 
 
 def dJdW(weights_vector):
   delta = h(weights_vector, x) - y
-  return np.dot(delta, x.T) / x.shape[1]
+  return np.dot(delta, x.T) / samples
 
 
 # Create a vector of random weights.
 weights = np.random.rand(1, x.shape[0])
+initial_weights = np.copy(weights)
 
 
 # Setup the learning rate and iterate on the weights.
 learning_rate = 1e-5
-iterations = 1e4
+iterations = 1e5
 costs = []
 while True:
-  current_cost = logistic_cost(weights)
-  print 'cost:', current_cost
-  costs.append(current_cost)
+  costs.append(logistic_cost(weights))
   # Improve the weights.
   partial_derivatives = dJdW(weights)
   weights -= learning_rate * partial_derivatives
@@ -70,11 +71,9 @@ while True:
     break
 
 
-plt.plot(costs, 'rx')
+# Plot cost v iteration.
+plt.plot(costs)
 plt.show()
-
-
-import sys; sys.exit()
 
 
 # Plot the dataset.
@@ -85,3 +84,7 @@ for i in range(len(x1[0])):
     marker = 'rx'
   plt.plot(x1[0][i], x2[0][i], marker)
 plt.show()
+
+
+print initial_weights
+print weights
